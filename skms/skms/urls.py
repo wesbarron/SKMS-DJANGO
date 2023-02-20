bf01da4fc225339ee9fcd00f8097903992826112
Katie Alley <112027177+katiealleyvt@users.noreply.github.com>
2023-02-19 20:32:41 -0500

    Implemented Posts
    -create post
    -view posts on discussion home page and post page

--- skms/skms/urls.py
+++ skms/skms/urls.py
@@ -2,8 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('sign-in/', views.index),
+    path('', views.index),
+    path('sign-in/', views.signIn),
     path('sign-up/', views.createAccount),
     path('profile/', views.profile),
-    path('forum/', views.forum),
+    path('forum/', views.forum, name='forum'),
+    path('forum/create-post/', views.renderCreatePost),
+    path('forum/create-post/create', views.createPost),
+    path('forum/post/<int:post_id>/', views.post, name='post'),
+    
 ]
--- skms/skms/views.py
+++ skms/skms/views.py
@@ -4,13 +4,17 @@ from django.contrib import messages
 from django.contrib.auth.models import User  
 from django.contrib.auth.forms import UserCreationForm  
 from .signUpForm import username_clean, email_clean
-
+from skms.models import *
+from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.from django.http import HttpResponse
 
-
 def index(request):
+    #if user is logged in, return our designated home page, else return to sign in 
+    return redirect('sign-in/')
+def signIn(request):
     return render(request, 'sign-in.html')
 
 def createAccount(request):
@@ -37,4 +41,37 @@ def profile(request):
     return render(request, "profile.html")
 
 def forum(request):
-    return render(request, "discussion-home.html")
+    posts = Post.objects.all()
+    return render(request, "discussion-home.html", {'posts':posts})
+
+def post(request, post_id):
+    post = Post.objects.get(id=post_id)
+    return render(request, "post.html", {'post':post})
+
+
+def renderCreatePost(request):
+    return render(request,"create-post.html")
+
+def createTestPost():
+    author = User.objects.get(username='katieavt')
+    post1 = Post(title='This is a test post',
+        content='This is test content.',
+        author=author,
+        datetime = timezone.now(),
+        subject="TestSubject"
+    )
+    post1.save()
+    return post1
+
+@login_required
+def createPost(request):
+    if request.method == 'POST': 
+        title = request.POST['title']
+        content = request.POST['description'] 
+        author = request.user
+        datetime = timezone.now()
+        subject = "Null Subject"
+        
+        newPost = Post(title=title, content=content, author=author, datetime=datetime, subject=subject)
+        newPost.save()
+        return redirect('forum')
