79244a7f9334eaf5379984aac92896412a34641a
Katie Alley <112027177+katiealleyvt@users.noreply.github.com>
2023-02-19 20:54:29 -0500

    Revert "Implemented Posts"
    This reverts commit bf01da4fc225339ee9fcd00f8097903992826112.

--- skms/skms/models.py
+++ skms/skms/models.py
@@ -1,26 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-class Post(models.Model):
-    #likes, comments, title, content, userUploaded, date, time, subject, subscriptions
-    title = models.CharField(max_length=200)
-    content = models.TextField()
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    datetime = models.DateTimeField(auto_now_add=True)
-    likes = models.IntegerField(default=0)
-    subject = models.CharField(max_length=100)
-    subscriptions = models.ManyToManyField('auth.User', related_name='subscribed_posts')
-    comments = models.ManyToManyField('Comment', related_name='post_comments')
-    
-    def __str__(self):
-        return self.title
-
-class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='post_comments')
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    content = models.TextField()
-    datetime = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f'{self.author.username} - {self.content}'
--- skms/skms/urls.py
+++ skms/skms/urls.py
@@ -2,13 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('sign-in/', views.signIn),
+    path('sign-in/', views.index),
     path('sign-up/', views.createAccount),
     path('profile/', views.profile),
-    path('forum/', views.forum, name='forum'),
-    path('forum/create-post/', views.renderCreatePost),
-    path('forum/create-post/create', views.createPost),
-    path('forum/post/<int:post_id>/', views.post, name='post'),
-    
+    path('forum/', views.forum),
 ]
